2560fdbcd72d79c0e0a54b2429edace19d7ca7eb
Miguel Camilo <miguelcamilo48@gmail.com>
2023-05-16 18:37:52 -0400

    ADDING Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
+
+import Login from './pages/Login'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
   )
 }
 
