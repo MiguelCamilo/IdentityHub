691cfc7722f8e18294a020929189c0b46be8476d
Miguel Camilo <miguelcamilo48@gmail.com>
2023-05-17 00:16:48 -0400

    ADDING login functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import Inputs from "../components/Inputs";
 import Img from "../components/Img";
 import Buttons from "../components/Buttons";
@@ -5,6 +7,14 @@ import Buttons from "../components/Buttons";
 import "../App.css";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+
+    }
 	return (
 		<>
 			{/* Global Container */}
@@ -18,42 +28,47 @@ const Login = () => {
 						<p className="max-w-sm mb-5 font-sans font-light text-gray-600">
 							Welcome back, login to your account!
 						</p>
-						<Inputs type="email" placeholder="Email" required />
-						<Inputs type="password" placeholder="Password" required />
 
-						{/* Middle Content */}
-						<div className="flex flex-col items-center justify-between my-6 md:flex-row md:space-y-0">
-							<div className="flex flex-col text-center font-thin text-indigo-700 hover:text-indigo-300 hover:duration-300 cursor-pointer">
-								<span className="text-gray-500 text-sm font-thin">
-									Don't have an account?
-								</span>{" "}
-								<span className="underline decoration-solid decoration-1 mb-2 sm:mb-0">
-									Create an account
-								</span>
-							</div>
+						<form onSubmit={handleSubmit}>
+							<Inputs type="email" placeholder="Email" required />
+							<Inputs type="password" placeholder="Password" required />
 
-							<div className="font-thin text-indigo-700 hover:text-indigo-300 hover:duration-300 cursor-pointer">
-								Forgot password?
+							{/* Middle Content */}
+							<div className="flex flex-col items-center justify-between my-6 md:flex-row md:space-y-0">
+								<div className="flex flex-col text-center font-thin text-indigo-700 hover:text-indigo-300 hover:duration-300 cursor-pointer">
+									<span className="text-gray-500 text-sm font-thin">
+										Don't have an account?
+									</span>{" "}
+									<span className="underline decoration-solid decoration-1 mb-2 sm:mb-0">
+										Create an account
+									</span>
+								</div>
+
+								<div className="font-thin text-indigo-700 hover:text-indigo-300 hover:duration-300 cursor-pointer">
+									Forgot password?
+								</div>
 							</div>
-						</div>
-						<Buttons className="flex w-full justify-center rounded-md gradient-bg px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:opacity-90 hover:duration-300">
-							<span>Login</span>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="w-7"
-								viewBox="0 0 24 24"
-								strokeWidth="1.5"
-								stroke="#ffffff"
-								fill="none"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							>
-								<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-								<line x1={5} y1={12} x2={19} y2={12} />
-								<line x1={13} y1={18} x2={19} y2={12} />
-								<line x1={13} y1={6} x2={19} y2={12} />
-							</svg>
-						</Buttons>
+							<Buttons className="flex w-full justify-center rounded-md gradient-bg px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:opacity-90 hover:duration-300">
+								<span>Login</span>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="w-7"
+									viewBox="0 0 24 24"
+									strokeWidth="1.5"
+									stroke="#ffffff"
+									fill="none"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+								>
+									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+									<line x1={5} y1={12} x2={19} y2={12} />
+									<line x1={13} y1={18} x2={19} y2={12} />
+									<line x1={13} y1={6} x2={19} y2={12} />
+								</svg>
+							</Buttons>
+						</form>
+
+                        
 						{/* Border */}
 						<div className="mt-12 border-b border-b-gray-300" />
 						{/* Bottom Content */}
